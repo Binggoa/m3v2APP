6dafe56cb560fad058029d75c31ca526497177c8
Binggoa <47893508+Binggoa@users.noreply.github.com>
2024-01-10 22:11:55 +0800

    Add files via upload

---
+++ project/shop/assets/data/json/goodsDetailModel.js
@@ -0,0 +1,50 @@
+goodsDetailModleByCache = {
+	"Goods": {
+		"GoodsID": 49,
+		"GoodsName": "牛滑木色杆1.0MM半针配RS02系列芯拔帽中油笔",
+		"GoodsModel": "G-3126",
+		"DefaultPicURL": "http://res.genvana.cn/upload/goods/201906161026374064.jpg",
+		"GoodsSalePoint": "超级好用",
+		"GoodsTag":"新品,热销",
+		"ReductionStr":"购买该商品满50元减5元,满100元减10元，满100元减10元，满100元减10元，满100元减10元",
+		"GoodsUnitTemplateDes":"1245PCS/箱，145PCS/盒，1PCS/PCS",
+		"GoodsDesc": "%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027479513.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_03.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027487433.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_01.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027493361.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_02.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cbr%2F%3E%3C%2Fp%3E",
+	},
+	"GoodsSku": [{
+		"GoodsAttributesValueName": "黑色",
+	},{
+		"GoodsAttributesValueName": "红色",
+	},{
+		"GoodsAttributesValueName": "白色",
+	}],
+	"GetGoodsPicURL": [{
+		"PicURL": "http://res.genvana.cn/upload/goods/201906161026374064.jpg",
+	}, {
+		"PicURL": "http://res.genvana.cn/upload/goods/201906161026427833.jpg",
+	}, {
+		"PicURL": "http://res.genvana.cn/upload/goods/201906161026466437.jpg",
+	}, {
+		"PicURL": "http://res.genvana.cn/upload/goods/201906161026501886.jpg",
+	}],
+	"GoodsUnitTemplateList": [{
+		"GoodsUnitName": "PCS",
+		"GoodsUnitTemplateName": "12*12*12=1728",
+		"PCS": 1,
+		"UnitTemplateDes": "1PCS/PCS",
+	}, {
+		"GoodsUnitName": "盒",
+		"GoodsUnitTemplateName": "12*12*12=1728",
+		"PCS": 12,
+		"UnitTemplateDes": "12PCS/盒",
+	}, {
+		"GoodsUnitName": "条",
+		"GoodsUnitTemplateName": "12*12*12=1728",
+		"PCS": 144,
+		"UnitTemplateDes": "144PCS/条",
+	}, {
+		"GoodsUnitName": "箱",
+		"GoodsUnitTemplateName": "12*12*12=1728",
+		"PCS": 1728,
+		"UnitTemplateDes": "1728PCS/箱",
+	}]
+}
